72196f55a7b691befd9435c8a996aa06c5b8d6bb
Sayan Ghosh <99132893+sayanghoshofficial@users.noreply.github.com>
2023-06-07 14:51:27 +0530

    create repo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from "./Component";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Styles from "../Styles/navbar.module.css";
+import Menu from "../Assets/Icons/menu.png";
+import Cross from "../Assets/Icons/close.png"
+
+const Navbar = () => {
+    const [menu, setMenu] = useState(false)
+
+    const toggleMenu=()=>{
+        setMenu(!menu);
+    }
+  return (
+    <div className={Styles.main}>
+      <div className={Styles.navbar}>
+        <div className="logo">
+          <a href="#">Welcome to my StartUp</a>
+        </div>
+        <ul className={Styles.link}>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+        <a href="#" className={Styles.actionBtn}>
+          Get Started
+        </a>
+        <div  className={Styles.toggleBtn}>
+          <img alt="menu" src={menu?Menu:Cross} onClick={toggleMenu}/>
+        </div>
+      </div>
+      <div style={menu?{display:'none'}:null} className={Styles.dropDownMenu}>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+        <li>
+          <a href="#" className={Styles.actionBtn}>
+            Get Started
+          </a>
+        </li>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
