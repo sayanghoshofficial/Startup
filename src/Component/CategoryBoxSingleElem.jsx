cf14d850340606ae80c6c489c3d63df253df25c0
Sayan Ghosh <99132893+sayanghoshofficial@users.noreply.github.com>
2023-06-08 09:28:35 +0530

    update catagory

--- src/Component/CategoryBoxSingleElem.jsx
+++ src/Component/CategoryBoxSingleElem.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 import Style from "../Styles/category2.module.css";
 
 const CategoryBoxSingleElem = (details) => {
-    console.log(details);
   return (
     <div className={Style.singelBox}>
       <b>{details.details}</b>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryBoxSingleElem
+export default CategoryBoxSingleElem;
